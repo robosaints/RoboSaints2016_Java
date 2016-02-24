c31fb49bfcde96ed964996d543be8d699bb1c351
John Doe <johndoe@example.com>
2016-02-23 18:12:16 -0600

    modified fire to 65% from 85%

--- src/org/usfirst/frc/team4762/robosaints16/commands/Fire.java
+++ src/org/usfirst/frc/team4762/robosaints16/commands/Fire.java
@@ -31,7 +31,7 @@ public class Fire extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	this.setMotors(-.85d, .85d);
+    	this.setMotors(-.65d, .65d);
     }
 
     // Make this return true when this Command no longer needs to run execute()
